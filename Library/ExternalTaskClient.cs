880cf0a410f9f1b825c836a7c52d5d80ea73b753
Chris Håland <chris.haland@outlook.com>
2022-06-03 23:42:20 +0200

    Add retry method for external task handlers

--- Library/ExternalTaskClient.cs
+++ Library/ExternalTaskClient.cs
@@ -160,6 +160,11 @@ internal class LockedExternalTaskDto
     /// A JSON object containing a property for each of the requested variables.
     /// </summary>
     public IDictionary<string, VariableDto> Variables { get; set; } = new Dictionary<string, VariableDto>();
+
+    /// <summary>
+    /// The number of retries the task currently has left.
+    /// </summary>
+    public int? Retries { get; set; }
 }
 
 internal class FailExternalTaskDto
@@ -188,6 +193,16 @@ internal class FailExternalTaskDto
     /// A JSON object containing local variable key-value pairs.Local variables are set only in the scope of external task.
     /// </summary>
     public IDictionary<string, VariableDto>? LocalVariables { get; set; }
+
+    /// <summary>
+    /// A number of how often the task should be retried. Must be >= 0. If this is 0, an incident is created and the task cannot be fetched anymore unless the retries are increased again. The incident's message is set to the errorMessage parameter.
+    /// </summary>
+    public int? Retries { get; set; }
+
+    /// <summary>
+    /// A timeout in milliseconds before the external task becomes available again for fetching. Must be >= 0.
+    /// </summary>
+    public long? RetryTimeout { get; set; }
 }
 
 internal class CompleteExternalTaskDto 
